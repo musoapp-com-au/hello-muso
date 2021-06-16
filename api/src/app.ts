01ddea6427d2ef0308014d33eb45178613e1a1f5
JT <justin@justintaylor.net>
2021-06-16 17:56:08 +1000

    Add and configure dotenv

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,3 +1,10 @@
+
+import dotenv from 'dotenv';
+const dotenvResult = dotenv.config();
+if (dotenvResult.error) {
+    throw dotenvResult.error;
+}
+
 import express from "express";
 import * as http from "http";
 
