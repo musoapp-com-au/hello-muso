e1d375854d0ca71c157496a3d629f96c17d28e43
JT <justin@justintaylor.net>
2021-06-23 13:34:28 +1000

    Add connection retry + logging for MongDB

--- api/src/common/datastores/mongoose.datastore.ts
+++ api/src/common/datastores/mongoose.datastore.ts
@@ -6,7 +6,6 @@ const log: debug.IDebugger = debug('app:mongoose-datastore');
 
 class MongooseDatastore {
 
-
     private mongooseOptions = {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -21,9 +20,17 @@ class MongooseDatastore {
     }
 
     connectWithRetry = () => {
-
+        
         log(`Accessing data base at - ${process.env.DATABASE_URL}`)
         mongoose.connect(process.env.DATABASE_URL, this.mongooseOptions)
+        . then(() => {
+            log(`Connection to ${process.env.DATABASE_URL} succeeded`)
+        })
+        .catch((error) => {
+            const retryWaitTime = 10; //seconds
+            log(`Datastore connection failed - retrying after ${retryWaitTime} seconds:`, error);
+            setTimeout(this.connectWithRetry, retryWaitTime * 1000);
+        });
     }
 }
 
